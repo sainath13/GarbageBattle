c9ac7673f88dd72f9c7e99623322251a9701447a
sainathl <latkarsainath@gmail.com>
2024-02-25 11:58:36 +0530

    [Add] refactor changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'package:garbage_sorting/app_barcode_scanner_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:widget_circular_animator/widget_circular_animator.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chat_bubbles/chat_bubbles.dart';
-import 'dart:math';
 
 void main() {
   runApp(
@@ -17,24 +15,6 @@ void main() {
     ),
   );
 }
-
-Duration duration = new Duration();
-Duration position = new Duration();
-bool isPlaying = false;
-bool isLoading = false;
-bool isPause = false;
-
-const colorizeColors = [
-  Colors.purple,
-  Colors.blue,
-  Colors.yellow,
-  Colors.red,
-];
-
-const colorizeTextStyle = TextStyle(
-  fontSize: 50.0,
-  fontFamily: 'Horizon',
-);
 
 enum GarbageType { wet, dry, sanitary, ewaste }
 
@@ -59,180 +39,6 @@ List<Item> _items = [
   ),
 ];
 
-List<Item> _itemsBackup = [
-  const Item(
-    name: 'Aluminium can',
-    totalPriceCents: 1299,
-    uid: '1',
-    imageProvider: AssetImage('assets/alum_can.png'),
-    garbageType: GarbageType.dry,
-    incorrectMessageDescription:
-        "Oops! Looks like this can needs a different destination. Think about where you'd recycle it.",
-  ),
-  const Item(
-    name: 'Band-Aids',
-    totalPriceCents: 799,
-    uid: '2',
-    imageProvider: AssetImage('assets/band_aids.png'),
-    garbageType: GarbageType.sanitary,
-    incorrectMessageDescription:
-        "Uh-oh! Seems like this item is more suited for a specific bin. Consider its material and its journey after disposal.",
-  ),
-  const Item(
-    name: 'Burger',
-    totalPriceCents: 1499,
-    uid: '3',
-    imageProvider: AssetImage('assets/burger.png'),
-    garbageType: GarbageType.wet,
-    incorrectMessageDescription:
-        "Oops, looks like this burger's journey has been cut short! Consider where its remaining 'half' belongs",
-  ),
-  const Item(
-    name: 'Cardboard Box',
-    totalPriceCents: 1499,
-    uid: '4',
-    imageProvider: AssetImage('assets/cardboard_box.png'),
-    garbageType: GarbageType.dry,
-    incorrectMessageDescription:
-        "Hmm, this one might need a sturdier home. Think about where you'd put it for a new life.",
-  ),
-  const Item(
-    name: 'Charging Cable',
-    totalPriceCents: 1499,
-    uid: '5',
-    imageProvider: AssetImage('assets/charging_cable.png'),
-    garbageType: GarbageType.ewaste,
-    incorrectMessageDescription:
-        "Whoopsie! This item needs a charge of its own, but in a different bin. Consider its technological nature.",
-  ),
-  const Item(
-    name: 'Disposable Cup',
-    totalPriceCents: 1499,
-    uid: '6',
-    imageProvider: AssetImage('assets/disposable_cup.png'),
-    garbageType: GarbageType.dry,
-    incorrectMessageDescription:
-        "Oh dear, this cup's journey might be a bit different than you think. Reflect on its composition.",
-  ),
-  const Item(
-    name: 'Egg Shells',
-    totalPriceCents: 1499,
-    uid: '7',
-    imageProvider: AssetImage('assets/egg_shells.png'),
-    garbageType: GarbageType.wet,
-    incorrectMessageDescription:
-        "Hm, this one's a bit 'shell'-shocked! Imagine where it belongs after its 'cracking' adventure.",
-  ),
-  const Item(
-    name: 'Food Leftover',
-    totalPriceCents: 1499,
-    uid: '8',
-    imageProvider: AssetImage('assets/food_leftover.png'),
-    garbageType: GarbageType.wet,
-    incorrectMessageDescription:
-        "Ah, leftovers from a journey! But perhaps a different destination awaits them. Consider their origins.",
-  ),
-  const Item(
-    name: 'Fruit Scraps',
-    totalPriceCents: 1499,
-    uid: '9',
-    imageProvider: AssetImage('assets/fruit_scraps.png'),
-    garbageType: GarbageType.wet,
-    incorrectMessageDescription:
-        "Hmm, these scraps may lead to a fruitful destination elsewhere. Ponder their potential.",
-  ),
-  const Item(
-    name: 'Game Console',
-    totalPriceCents: 1499,
-    uid: '10',
-    imageProvider: AssetImage('assets/game_console.png'),
-    garbageType: GarbageType.ewaste,
-    incorrectMessageDescription:
-        "Whoa, a gaming device! But its journey might be to a different bin. Imagine where it 'resets'.",
-  ),
-  const Item(
-    name: 'Keyboard',
-    totalPriceCents: 1499,
-    uid: '11',
-    imageProvider: AssetImage('assets/keyboard.png'),
-    garbageType: GarbageType.ewaste,
-    incorrectMessageDescription:
-        "Oops, seems like this keyboard needs a different key to its journey. Reflect on its functionality.",
-  ),
-  const Item(
-    name: 'Mask',
-    totalPriceCents: 1499,
-    uid: '12',
-    imageProvider: AssetImage('assets/mask.png'),
-    garbageType: GarbageType.sanitary,
-    incorrectMessageDescription:
-        "Ah, a mask! But perhaps a different bin awaits it for a new journey. Consider its protective purpose.",
-  ),
-  const Item(
-    name: 'Mouse',
-    totalPriceCents: 1499,
-    uid: '13',
-    imageProvider: AssetImage('assets/mouse.png'),
-    garbageType: GarbageType.ewaste,
-    incorrectMessageDescription:
-        "Squeak! This mouse's journey might lead it to a different bin. Imagine where it 'clicks'.",
-  ),
-  const Item(
-    name: 'Newspapers',
-    totalPriceCents: 1499,
-    uid: '14',
-    imageProvider: AssetImage('assets/newspaper.png'),
-    garbageType: GarbageType.dry,
-    incorrectMessageDescription:
-        "Hm, this news needs a new destination. Think about where it belongs after being 'read'.",
-  ),
-  const Item(
-    name: 'Plastic Bottle',
-    totalPriceCents: 1499,
-    uid: '15',
-    imageProvider: AssetImage('assets/pastic_bottle.png'),
-    garbageType: GarbageType.dry,
-    incorrectMessageDescription:
-        "Oh, a bottle! But where does it go after quenching its thirst? Imagine its next adventure.",
-  ),
-  const Item(
-    name: 'Broken Phone',
-    totalPriceCents: 1499,
-    uid: '16',
-    imageProvider: AssetImage('assets/phone.png'),
-    garbageType: GarbageType.ewaste,
-    incorrectMessageDescription:
-        "Uh-oh, this phone's journey might need a new 'connection'. Think about where its 'broken' parts should go for a new life",
-  ),
-  const Item(
-    name: 'Sanitary Napkins',
-    totalPriceCents: 1499,
-    uid: '17',
-    imageProvider: AssetImage('assets/sanitary_napkins.png'),
-    garbageType: GarbageType.sanitary,
-    incorrectMessageDescription:
-        "Whoops, this item might need a different destination. Reflect on its hygiene purpose.",
-  ),
-  const Item(
-    name: 'Tampons',
-    totalPriceCents: 1499,
-    uid: '18',
-    imageProvider: AssetImage('assets/tampons.png'),
-    garbageType: GarbageType.sanitary,
-    incorrectMessageDescription:
-        "Hmm, these might need a different bin for their next 'cycle'. Imagine where they belong.",
-  ),
-  const Item(
-    name: 'Tea Bags',
-    totalPriceCents: 1499,
-    uid: '19',
-    imageProvider: AssetImage('assets/tea_bags.png'),
-    garbageType: GarbageType.wet,
-    incorrectMessageDescription:
-        "Oops, these bags might need a different brew of a bin. Ponder their compostable potential",
-  )
-];
-
 @immutable
 class ExampleDragAndDrop extends StatefulWidget {
   const ExampleDragAndDrop({super.key});
@@ -243,11 +49,9 @@ class ExampleDragAndDrop extends StatefulWidget {
 
 class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
     with TickerProviderStateMixin {
-  final List<Customer> _people = [
-    Customer(
+  final List<Dustbin> _people = [
+    Dustbin(
       name: '    Wet waste    ',
-      imageProvider: const NetworkImage('https://flutter'
-          '.dev/docs/cookbook/img-files/effects/split-check/Avatar1.jpg'),
       garbageType: GarbageType.wet,
       color: Colors.green,
       icon: const Icon(
@@ -256,10 +60,8 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
         size: 60.0,
       ),
     ),
-    Customer(
+    Dustbin(
       name: '   Dry waste     ',
-      imageProvider: const NetworkImage('https://flutter'
-          '.dev/docs/cookbook/img-files/effects/split-check/Avatar2.jpg'),
       garbageType: GarbageType.dry,
       color: Colors.blue,
       icon: const Icon(
@@ -268,10 +70,8 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
         size: 60.0,
       ),
     ),
-    Customer(
+    Dustbin(
       name: 'Sanitary waste',
-      imageProvider: const NetworkImage('https://flutter'
-          '.dev/docs/cookbook/img-files/effects/split-check/Avatar3.jpg'),
       garbageType: GarbageType.sanitary,
       color: Colors.red,
       icon: const Icon(
@@ -280,10 +80,8 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
         size: 60.0,
       ),
     ),
-    Customer(
+    Dustbin(
       name: '     E waste       ',
-      imageProvider: const NetworkImage('https://flutter'
-          '.dev/docs/cookbook/img-files/effects/split-check/Avatar3.jpg'),
       garbageType: GarbageType.ewaste,
       color: Colors.grey,
       icon: const Icon(
@@ -296,19 +94,19 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
 
   final GlobalKey _draggableKey = GlobalKey();
 
-  void _itemDroppedOnCustomerCart({
+  void _itemDroppedOnDustbin({
     required Item item,
-    required Customer customer,
+    required Dustbin dustbin,
   }) {
     setState(() {
-      customer.items.add(item);
+      dustbin.items.add(item);
       _items.removeWhere((element) => element.uid == item.uid);
     });
   }
 
   void _collectibleCollected({
     required Item item,
-    required Customer customer,
+    required Dustbin customer,
   }) {
     setState(() {
       customer.items.add(item);
@@ -346,17 +144,17 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
       children: [
         Expanded(
           flex: 8,
-          child: _buildMenuList(),
+          child: _buildGarbageList(),
         ),
         Expanded(
           flex: 2,
-          child: _buildPeopleRow(),
+          child: _buildDustbinRow(),
         ),
       ],
     );
   }
 
-  Widget _buildMenuList() {
+  Widget _buildGarbageList() {
     if (_people[0].items.length == 1) {
       return _buildCongratulationsScreen(_people[0]);
     } else if (_people[1].items.length == 1) {
@@ -397,9 +195,7 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
     }
   }
 
-  var _code = "hello";
-  Widget _buildCongratulationsScreen(Customer customer) {
-    final now = new DateTime.now();
+  Widget _buildCongratulationsScreen(Dustbin customer) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -431,58 +227,7 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
                 ),
                 // const SizedBox(width: 200),
                 CollectibleCardWidget(),
-                Card(
-                  elevation: 50,
-                  shadowColor: Colors.black,
-                  child: SizedBox(
-                    width: 300,
-                    height: 500,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            BubbleSpecialTwo(
-                              text: 'Mom, who is this stunning cheetah',
-                              isSender: true,
-                              color: Color.fromARGB(255, 110, 110, 198),
-                              tail: true,
-                              sent: true,
-                            ),
-                            BubbleSpecialOne(
-                              text:
-                                  'Honey he is Zephyr. He roams around planets and eats rabbits. You have got it as a card reward for collecting all sanitary garbage correctly.',
-                              isSender: false,
-                              color: Color(0xFF1B97F3),
-                              textStyle: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                            BubbleSpecialTwo(
-                              text:
-                                  'WOw mom thats awesome, I dont want to loose him, how do I save him',
-                              isSender: true,
-                              color: Color.fromARGB(255, 110, 110, 198),
-                              tail: true,
-                              sent: true,
-                            ),
-                            BubbleSpecialOne(
-                              text:
-                                  'Add it to your google wallet using the button below',
-                              isSender: false,
-                              color: Color(0xFF1B97F3),
-                              textStyle: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                ChatWidget(),
                 const SizedBox(height: 20),
               ],
             ),
@@ -507,25 +252,6 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
               },
               child: const Text('Add to Google Wallet'),
             ),
-            // Column(
-            //   children: [
-            //     Expanded(
-            //       flex: 4,
-            //       child: AppBarcodeScannerWidget.defaultStyle(
-            //         resultCallback: (String code) {
-            //           setState(() {
-            //             _code = code;
-            //           });
-            //         },
-            //         openManual: true,
-            //       ),
-            //     ),
-            //     Expanded(
-            //       flex: 1,
-            //       child: Container(),
-            //     ),
-            //   ],
-            // )
           ],
         ),
       ),
@@ -536,26 +262,6 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
     return DialogRoute<void>(
       context: context,
       builder: (BuildContext context) {
-        // return Column(
-        //   children: [
-        //     Expanded(
-        //       flex: 4,
-        //       child: AppBarcodeScannerWidget.defaultStyle(
-        //         resultCallback: (String code) {
-        //           print("Scanned text is " + code);
-        //           setState(() {
-        //             _code = code;
-        //           });
-        //         },
-        //         openManual: true,
-        //       ),
-        //     ),
-        //     Expanded(
-        //       flex: 1,
-        //       child: Container(),
-        //     ),
-        //   ],
-        // );
         return AlertDialog(
           title: const Text('AlertDialog Title'),
           content: SingleChildScrollView(
@@ -565,9 +271,7 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
               child: AppBarcodeScannerWidget.defaultStyle(
                 resultCallback: (String code) {
                   print("Scanned text is " + code);
-                  setState(() {
-                    _code = code;
-                  });
+                  setState(() {});
                 },
                 openManual: true,
               ),
@@ -612,7 +316,7 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
     );
   }
 
-  Widget _buildPeopleRow() {
+  Widget _buildDustbinRow() {
     return Container(
       padding: const EdgeInsets.symmetric(
         horizontal: 2.5,
@@ -624,7 +328,7 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
     );
   }
 
-  Widget _buildPersonWithDropZone(Customer customer) {
+  Widget _buildPersonWithDropZone(Dustbin customer) {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(
@@ -640,9 +344,9 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
           },
           onAccept: (item) {
             if (item.garbageType == customer.garbageType) {
-              _itemDroppedOnCustomerCart(
+              _itemDroppedOnDustbin(
                 item: item,
-                customer: customer,
+                dustbin: customer,
               );
             } else {
               showModalBottomSheet<Item>(
@@ -679,6 +383,67 @@ class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
               );
             }
           },
+        ),
+      ),
+    );
+  }
+}
+
+class ChatWidget extends StatelessWidget {
+  const ChatWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 50,
+      shadowColor: Colors.black,
+      child: SizedBox(
+        width: 300,
+        height: 500,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                BubbleSpecialTwo(
+                  text: 'Mom, who is this stunning cheetah',
+                  isSender: true,
+                  color: Color.fromARGB(255, 110, 110, 198),
+                  tail: true,
+                  sent: true,
+                ),
+                BubbleSpecialOne(
+                  text:
+                      'Honey he is Zephyr. He roams around planets and eats rabbits. You have got it as a card reward for collecting all sanitary garbage correctly.',
+                  isSender: false,
+                  color: Color(0xFF1B97F3),
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                BubbleSpecialTwo(
+                  text:
+                      'WOw mom thats awesome, I dont want to loose him, how do I save him',
+                  isSender: true,
+                  color: Color.fromARGB(255, 110, 110, 198),
+                  tail: true,
+                  sent: true,
+                ),
+                BubbleSpecialOne(
+                  text: 'Add it to your google wallet using the button below',
+                  isSender: false,
+                  color: Color(0xFF1B97F3),
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -1010,7 +775,7 @@ class CustomerCart extends StatelessWidget {
     this.hasItems = false,
   });
 
-  final Customer customer;
+  final Dustbin customer;
   final bool highlighted;
   final bool hasItems;
 
@@ -1214,10 +979,9 @@ class Item {
       '\$${(totalPriceCents / 100.0).toStringAsFixed(2)}';
 }
 
-class Customer {
-  Customer({
+class Dustbin {
+  Dustbin({
     required this.name,
-    required this.imageProvider,
     required this.garbageType,
     required this.color,
     required this.icon,
@@ -1225,7 +989,6 @@ class Customer {
   }) : items = items ?? [];
 
   final String name;
-  final ImageProvider imageProvider;
   List<Item> items;
   final GarbageType garbageType;
   final Color color;
